774b2d3f16567506163bf54a3cfe88d835603af7
heliannuuthus <118797425+heliannuuthus@users.noreply.github.com>
2026-08-25 00:43:28 +0800

    fix(build): own utility declaration types (#34)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export const cn = (...inputs: Parameters<typeof clsx>) => {
-  return twMerge(clsx(inputs));
+export type ClassNameValue =
+  | ClassNameValue[]
+  | Record<string, unknown>
+  | ((...args: never[]) => unknown)
+  | string
+  | number
+  | bigint
+  | null
+  | boolean
+  | undefined;
+
+export const cn = (...inputs: ClassNameValue[]): string => {
+  return twMerge(clsx(inputs as never[]));
 };
